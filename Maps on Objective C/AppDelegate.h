29ebdd64b1ab1d00f0d1392cc9a7c0d47b030cd6
Karlo Pagtakhan <kidap@yahoo.com>
2016-01-30 23:00:58 -0500

    Initial Commit

---
+++ Maps on Objective C/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Maps on Objective C
+//
+//  Created by Karlo Pagtakhan on 01/30/2016.
+//  Copyright © 2016 AccessIT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Maps on Objective C/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Maps on Objective C
+//
+//  Created by Karlo Pagtakhan on 01/30/2016.
+//  Copyright © 2016 AccessIT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
